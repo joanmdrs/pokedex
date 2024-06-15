ea86f3e54b4b4e85b16e8a7b6fe39cdc6b46820b
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-06-15 07:50:58 -0300

    alterando o arquivo principal da pokedex

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      Pokedex
+      
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
