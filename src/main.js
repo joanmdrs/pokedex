a7b356a49683e0102a5127eae3d3ca467e4075d6
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-06-15 20:39:09 -0300

    reformulando o corpo do app para incluir apenas as rotas, e adicionando o objeto router ao app

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import router from './router/index'
+
 
 loadFonts()
 
 createApp(App)
+  .use(router)
   .use(vuetify)
   .mount('#app')
